4556af6ce0a2a803d7b4575c867ed3510d0a0356
Oliver Cook <oliverjcook3d@gmail.com>
2020-02-24 17:50:05 +1100

    updated CRUD and basic protfolio functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,69 +5,48 @@
       title: "Roy Batty",
       subtitle: "Blade Runner Project",
       content: "Roy Batty from  Blade Runner Portrait study ",
-      main_image: "https://cdnb.artstation.com/p/assets/images/images/021/201/063/large/oliver-cook-wip-54.jpg?1570750748",
-      thumb_image: "https://via.placeholder.com/350")
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582514804/3dPortfolio/oliver-cook-wip-54_dsu8l1.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582522145/3dPortfolio/roy_batty_thumbnail_vycvfz.jpg")
 
 
-      Portfolio.create!(
-      title: "Project2",
-      subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+
 
 
       Portfolio.create!(
-      title: "Project3",
-      subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+      title: "Commander of the Lost Expedition",
+      subtitle: "3D Artwork",
+      content: "Created In Zbrush, Maya, rendered in Arnold.",
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520309/3dPortfolio/astronaut_front_urjw1z.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582514747/3dPortfolio/astronaught_thumbnail_wxkcar.jpg")
 
 
       Portfolio.create!(
-      title: "Project4",
-      subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+      title: "Captain Jennings",
+      subtitle: "3D Artwork",
+      content: "Created In Zbrush, Maya, rendered in Arnold.",
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582517932/3dPortfolio/oliver-cook-final-senior-minion_b1mfsk.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582518669/3dPortfolio/senior-minion_thumbnail_me31is.jpg")
 
 
       Portfolio.create!(
-      title: "Project5",
+      title: "Doom Hell Baron",
       subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+      content: "Created In Zbrush, Maya, rendered in Arnold.",
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520614/3dPortfolio/oliver-cook-hell-baron-01b_g1kdf7.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582519304/3dPortfolio/hell-baron_thumbnail_350_hg7sth.jpg")
 
 
       Portfolio.create!(
-      title: "Project6",
-      subtitle: "Awesome Artwork",
+      title: "Ilia",
+      subtitle: "3D Artwork",
       content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      # main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520522/3dPortfolio/ilia_front_y7t3sf.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582518720/3dPortfolio/ilia_thumnail_mlxc7d.jpg")
 
 
       Portfolio.create!(
-      title: "Project7",
+      title: "Bad Cooper",
       subtitle: "Awesome Artwork",
       content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      # main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
-
-
-      Portfolio.create!(
-      title: "Project8",
-      subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      # main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
-
-
-      Portfolio.create!(
-      title: "Project9",
-      subtitle: "Awesome Artwork",
-      content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
-      # main_image: "placeholder",
-      thumb_image: "https://via.placeholder.com/350")
+      main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520590/3dPortfolio/oliver-cook-bx-cooper-01_zkc1kb.jpg",
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582519033/3dPortfolio/oliver-cook-bad_cooper_thumbnail_ctngyu.jpg")
