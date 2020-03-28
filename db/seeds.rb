1969c2a81aa0c7fd0c712510215b4d1c38f8ee48
Oliver Cook <oliverjcook3d@gmail.com>
2020-03-28 18:56:35 +1100

    wip css improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Portfolio.destroy_all
       subtitle: "Blade Runner Project",
       content: "Roy Batty from  Blade Runner Portrait study ",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582514804/3dPortfolio/oliver-cook-wip-54_dsu8l1.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582522145/3dPortfolio/roy_batty_thumbnail_vycvfz.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,w_300/v1582522145/3dPortfolio/roy_batty_thumbnail_vycvfz.jpg")
 
 
 
@@ -17,7 +17,7 @@ Portfolio.destroy_all
       subtitle: "3D Artwork",
       content: "Created In Zbrush, Maya, rendered in Arnold.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520309/3dPortfolio/astronaut_front_urjw1z.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582514747/3dPortfolio/astronaught_thumbnail_wxkcar.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1582514747/3dPortfolio/astronaught_thumbnail_wxkcar.jpg")
 
 
       Portfolio.create!(
@@ -25,21 +25,21 @@ Portfolio.destroy_all
       subtitle: "3D Artwork",
       content: "Created In Zbrush, Maya, rendered in Redshift.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1585273852/3dPortfolio/CybG_main_iuzoxd.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1585273852/3dPortfolio/CybG_thumb_ue39uw.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1585273852/3dPortfolio/CybG_thumb_ue39uw.jpg")
 
       Portfolio.create!(
       title: "Sphynx Cat",
       subtitle: "3D Artwork",
       content: "Created In Zbrush, Maya, rendered in Redshift.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1585273852/3dPortfolio/Sphynx_Cat_main_lpd97e.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1585273852/3dPortfolio/Sphynx_Cat_THUMB_osowne.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1585273852/3dPortfolio/Sphynx_Cat_THUMB_osowne.jpg")
 
       Portfolio.create!(
       title: "Captain Jennings",
       subtitle: "3D Artwork",
       content: "Created In Zbrush, Maya, rendered in Arnold.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582517932/3dPortfolio/oliver-cook-final-senior-minion_b1mfsk.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582518669/3dPortfolio/senior-minion_thumbnail_me31is.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1582518669/3dPortfolio/senior-minion_thumbnail_me31is.jpg")
 
 
       Portfolio.create!(
@@ -47,7 +47,7 @@ Portfolio.destroy_all
       subtitle: "Awesome Artwork",
       content: "Created In Zbrush, Maya, rendered in Arnold.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520614/3dPortfolio/oliver-cook-hell-baron-01b_g1kdf7.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582519304/3dPortfolio/hell-baron_thumbnail_350_hg7sth.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1582519304/3dPortfolio/hell-baron_thumbnail_350_hg7sth.jpg")
 
 
       Portfolio.create!(
@@ -55,7 +55,7 @@ Portfolio.destroy_all
       subtitle: "3D Artwork",
       content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520522/3dPortfolio/ilia_front_y7t3sf.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582518720/3dPortfolio/ilia_thumnail_mlxc7d.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1582518720/3dPortfolio/ilia_thumnail_mlxc7d.jpg")
 
 
       Portfolio.create!(
@@ -63,4 +63,4 @@ Portfolio.destroy_all
       subtitle: "Awesome Artwork",
       content: "He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
       main_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582520590/3dPortfolio/oliver-cook-bx-cooper-01_zkc1kb.jpg",
-      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/v1582519033/3dPortfolio/oliver-cook-bad_cooper_thumbnail_ctngyu.jpg")
+      thumb_image: "https://res.cloudinary.com/haxxor70/image/upload/c_scale,h_300/v1582519033/3dPortfolio/oliver-cook-bad_cooper_thumbnail_ctngyu.jpg")
